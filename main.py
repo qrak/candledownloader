3ba240fe8168f47a5066e91c51abdcec524d3fa5
qrak <donqrakko@gmail.com>
2023-05-08 18:42:39 +0200

    - Timeframes loop added. It's possible to download multiple timeframes now. - Logging to file added, default: False

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 from classdir.candledownloader import CandleDownloader
 
-
 if __name__ == "__main__":
 
-    # Choose exchange
-    exchange_name = 'binance'
-    timeframe = '5m'
+    # Enable logging. Default: False
+    enable_logging = False
 
-    # Create lists of base symbols and quote symbols
+    # Choose exchange, tested on binance
+    exchange_name = 'binance'
+
+    # Create lists of base symbols, quote symbols, and timeframes
     base_symbols = ['BTC', 'ETH', 'ADA', 'DOT', 'XRP', 'SOL', 'MATIC', 'LTC', 'AVAX', 'LINK', 'ATOM', 'ETC']
     quote_symbols = ['USDT']
 
-    # Iterate over the base symbols and quote symbols and download candles for each pair
+    # timeframes available: '1m', '3m', '5m', '15m', '30m', '1h', '2h', '4h', '6h', '12h', '1d', '3d', '1w', '1M'
+    timeframes = ['5m', '15m', '1h', '1d', '1w', '1M']
+
+    # Iterate over the base symbols, quote symbols, and timeframes and download candles for each pair
     for base_symbol in base_symbols:
         for quote_symbol in quote_symbols:
-            pair_name = f"{base_symbol}/{quote_symbol}"
-            print(f"Downloading candles for {pair_name}... timeframe: {timeframe}")
-            candledownload = CandleDownloader(exchange_name=exchange_name, pair_name=pair_name, timeframe=timeframe)
-            candledownload.download_candles()
-            print(f"Finished downloading candles for {pair_name}\n")
+            for timeframe in timeframes:
+                pair_name = f"{base_symbol}/{quote_symbol}"
+                candledownload = CandleDownloader(exchange_name=exchange_name, pair_name=pair_name, timeframe=timeframe,
+                                                  log_to_file=enable_logging)
+                candledownload.logger.info(f"Downloading candles for {pair_name}... timeframe: {timeframe}")
+                candledownload.download_candles()
+                candledownload.logger.info(f"Finished downloading candles for {pair_name} with timeframe {timeframe}\n")
 
