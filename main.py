9efb2a2d3dddae3ae4324ddeb7bf656ea6bf9fa7
qrak <donqrakko@gmail.com>
2023-10-08 21:23:22 +0200

    Moved script documentation inside main execution block in main.py

--- main.py
+++ main.py
@@ -1,50 +1,49 @@
-"""
-This script serves as an entry point to initiate the process of downloading candlestick data
-for specified or all trading pairs from a given exchange, using the `CandleDataDownloader` class
-defined in the `classdir.candledownloader` module. The script offers the flexibility to specify
-the base and quote currencies, timeframes, an option to enable or disable logging, and whether to
-download data for all available trading pairs or just the specified ones.
-
-The `CandleDataDownloader` class interfaces with the `ccxt` library to interact with cryptocurrency
-exchanges and download candlestick data, which is further handled by the `CandleDownloader` class to
-organize and store the data into CSV files.
-
-Usage:
-    1. Adjust the `all_pairs` variable to True if you want to download candle data for all trading pairs
-       available on the exchange or to False if you want to download data for specific pairs.
-    2. If `all_pairs` is set to False, adjust the `base_symbols` list to include the desired base currencies.
-    3. Optionally, adjust the `quote_symbols` and `timeframes` list to customize the quote currencies
-       and timeframes respectively.
-    4. Set `enable_logging` to True if logging is desired.
-    5. Run the script.
-
-Parameters:
-    all_pairs (bool): Flag to indicate whether to download data for all trading pairs or just the specified ones.
-                      Default is True.
-    enable_logging (bool): Flag to enable or disable logging. Default is False.
-    base_symbols (list): List of base currencies for which to download candle data. Default list includes
-                         popular cryptocurrencies like BTC, ETH, ADA etc.
-    quote_symbols (list): List of quote currencies. Default is ['USDT'].
-    timeframes (list): List of timeframes for which to download candle data. Default is ['1h', '1d', '1w', '1M'].
-
-Classes:
-    CandleDataDownloader: Handles the configuration and initiation of candle data download process.
-    CandleDownloader: Manages the downloading, buffering, and storing of candle data.
-
-Dependencies:
-    - ccxt: Library for cryptocurrency trading.
-    - pandas: Data manipulation and analysis library.
-    - logging: Logging library for tracking events during runtime.
-
-
-Note:
-    Ensure the `ccxt` library is installed and the `classdir.candledownloader` module is accessible
-    from the script's location.
-"""
-
 from classdir.candledownloader import CandleDataDownloader
 
 if __name__ == "__main__":
+    """
+    This script serves as an entry point to initiate the process of downloading candlestick data
+    for specified or all trading pairs from a given exchange, using the `CandleDataDownloader` class
+    defined in the `classdir.candledownloader` module. The script offers the flexibility to specify
+    the base and quote currencies, timeframes, an option to enable or disable logging, and whether to
+    download data for all available trading pairs or just the specified ones.
+
+    The `CandleDataDownloader` class interfaces with the `ccxt` library to interact with cryptocurrency
+    exchanges and download candlestick data, which is further handled by the `CandleDownloader` class to
+    organize and store the data into CSV files.
+
+    Usage:
+        1. Adjust the `all_pairs` variable to True if you want to download candle data for all trading pairs
+           available on the exchange or to False if you want to download data for specific pairs.
+        2. If `all_pairs` is set to False, adjust the `base_symbols` list to include the desired base currencies.
+        3. Optionally, adjust the `quote_symbols` and `timeframes` list to customize the quote currencies
+           and timeframes respectively.
+        4. Set `enable_logging` to True if logging is desired.
+        5. Run the script.
+
+    Parameters:
+        all_pairs (bool): Flag to indicate whether to download data for all trading pairs or just the specified ones.
+                          Default is True.
+        enable_logging (bool): Flag to enable or disable logging. Default is False.
+        base_symbols (list): List of base currencies for which to download candle data. Default list includes
+                             popular cryptocurrencies like BTC, ETH, ADA etc.
+        quote_symbols (list): List of quote currencies. Default is ['USDT'].
+        timeframes (list): List of timeframes for which to download candle data. Default is ['1h', '1d', '1w', '1M'].
+
+    Classes:
+        CandleDataDownloader: Handles the configuration and initiation of candle data download process.
+        CandleDownloader: Manages the downloading, buffering, and storing of candle data.
+
+    Dependencies:
+        - ccxt: Library for cryptocurrency trading.
+        - pandas: Data manipulation and analysis library.
+        - logging: Logging library for tracking events during runtime.
+
+
+    Note:
+        Ensure the `ccxt` library is installed and the `classdir.candledownloader` module is accessible
+        from the script's location.
+    """
     # Enable logging. Default: False
     enable_logging = False
 
